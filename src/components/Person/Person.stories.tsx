8a57e070d63b1227a1d07986559d9dbaf0b01c02
Lourenco Werneck <lourencovw@gmail.com>
2022-04-17 19:53:58 -0300

    Create person component

--- src/components/Person/Person.stories.tsx
+++ src/components/Person/Person.stories.tsx
@@ -3,8 +3,17 @@ import Person from './Person';
 export default {
   title: "Person",
 };
+const randomPerson = {
+  _id: 'sd89fg7sdf8d8fg76d8f7g',
+  address: "888 Thatford Avenue, Chalfant, Alabama, 2716",
+  age: "60",
+  birthday: "1961-07-12T05:41:09",
+  name: "Lou Gardner",
+  phone_number: "(053) 8589246",
+  picture: "https://live.staticflickr.com/65535/52002904692_3277ccdb11_o.png",
+}
 
-export const Default = () => <Person />;
+export const Default = () => <Person  {...randomPerson} />;
 
 Default.story = {
   name: 'default',
--- src/components/Person/Person.test.tsx
+++ src/components/Person/Person.test.tsx
@@ -5,7 +5,16 @@ import Person from './Person';
 
 describe('<Person />', () => {
   test('it should mount', () => {
-    render(<Person />);
+    const randomPerson = {
+      _id: 'sd89fg7sdf8d8fg76d8f7g',
+      address: "888 Thatford Avenue, Chalfant, Alabama, 2716",
+      age: "60",
+      birthday: "1961-07-12T05:41:09",
+      name: "Lou Gardner",
+      phone_number: "(053) 8589246",
+      picture: "https://live.staticflickr.com/65535/52002904692_3277ccdb11_o.png",
+    }
+    render(<Person {...randomPerson}/>);
     
     const person = screen.getByTestId('Person');
 
