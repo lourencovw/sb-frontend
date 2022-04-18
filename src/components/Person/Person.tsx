76d24db30738ddb4e0dbe85a046abcad5e257951
Lourenco Werneck <lourencovw@gmail.com>
2022-04-18 15:11:10 -0300

    Unit test

--- src/components/Person/Person.tsx
+++ src/components/Person/Person.tsx
@@ -1,19 +1,18 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { IPerson } from '../../App.model';
 import './Person.css';
 
 
 const Person: FC<IPerson> = ({name, address, age, phone_number,picture}) => (
-  <div className="container">
-    <img className="user-avatar"
-      src={picture} />
+  <div className="container" data-testid="Person">
+    <img className="user-avatar" src={picture} data-testid="img"/>
 
     <div className="sub-container">
-      <div className="label">
+      <div className="label" data-testid="label">
         {name}, {age}, {phone_number}
       </div>
 
-      <p className="description">
+      <p className="description" data-testid="description">
         {address}
       </p>
     </div>
